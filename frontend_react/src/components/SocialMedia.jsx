0fe4a5be95d467367b1503e3c144563ede8645fe
kTz1 <catalin_simon90@yahoo.com>
2022-03-16 16:55:40 +0200

    remove .env file

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaFacebook } from 'react-icons/fa'
+
+const SocialMedia = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SocialMedia
---
+++ frontend_react/src/wrapper/AppWrap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavigationDots, SocialMedia } from '../components';
+
+const AppWrap = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AppWrap;
