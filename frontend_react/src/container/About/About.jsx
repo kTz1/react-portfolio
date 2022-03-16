fdb23789e07c194e3dbbf0f3a825ef0a3e1db028
kTz1 <catalin_simon90@yahoo.com>
2022-03-16 16:46:11 +0200

    connect about container to cms

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,21 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
+import { urlFor, client } from '../../client';
 
 import './about.scss';
 
-
-const abouts = [
-    {title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01},
-    {title: 'Web Design', description: 'I am a good web developer.', imgUrl: images.about02},
-    {title: 'UI/UX', description: 'I am a good web developer.', imgUrl: images.about03},
-    {title: 'Web animations', description: 'I am a good web developer.', imgUrl: images.about04},
-]
-
 const About = () => {
+    const [abouts, setAbouts] = useState([]);
+
+    useEffect(() => {
+        const query = '*[_type == "abouts"]';
+
+        client.fetch(query) 
+            .then((data) => setAbouts(data))
+    }, []);
+
     return (
         <>
-            <h2 className="head-text">I Know that <span>Good Development</span> <br /> means <span>Good Business</span>
+            <h2 className="head-text">I Know that <span>Good Design</span> <br />means <span>Good Business</span>
             </h2>
             <div className="app__profiles">
                 {abouts.map((about, index) => (
@@ -27,7 +29,7 @@ const About = () => {
                         className="app__profile-item"
                         key={about.title + index}
                     >   
-                        <img src={about.imgUrl} alt={about.title} />
+                        <img src={urlFor(about.imgUrl)} alt={about.title} />
                         <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
                         <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
                     </motion.div>
