c7cdc07619af53098527d35d543819431f60bac7
kTz1 <catalin_simon90@yahoo.com>
2022-03-18 16:39:11 +0200

    added motionWrap to all containers

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ReactTooltip from 'react-tooltip';
 import { motion } from 'framer-motion';
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 
 import './skills.scss';
@@ -87,4 +87,8 @@ const Skills = () => {
     )
 }
 
-export default AppWrap(Skills, 'skills');
+export default AppWrap(
+    MotionWrap(Skills, 'app__skills'),
+    'skills',
+    'app__whitebg'
+);
